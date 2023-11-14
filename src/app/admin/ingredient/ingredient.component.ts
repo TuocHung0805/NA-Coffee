90618e29a450ce6b631548e15ac5c3861e07ae4c
TuocHung0805 <139323813+TuocHung0805@users.noreply.github.com>
2023-11-14 23:33:20 +0700

    a

--- src/app/admin/ingredient/ingredient.component.ts
+++ src/app/admin/ingredient/ingredient.component.ts
@@ -33,7 +33,7 @@ export class IngredientComponent implements OnInit {
   ngOnInit(): void {
     this.getAllItem();
   }
-
+//
   getAllItem() {
     this.data.getAllItemIngredient().subscribe(
       (res) => {
