b61ca05a6c9134e562f4533b86e2f4993726eb28
TuocHung0805 <139323813+TuocHung0805@users.noreply.github.com>
2023-12-12 20:57:24 +0700

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -84,7 +84,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
